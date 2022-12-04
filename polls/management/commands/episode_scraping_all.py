22d9e84f5155dd0f7c88b8046dedd9a5c6be2630
Ahain5221 <dawkol1@st.amu.edu.pl>
2022-12-04 20:32:22 +0100

    fix
    ,

--- polls/management/commands/episode_scraping_all.py
+++ polls/management/commands/episode_scraping_all.py
@@ -2,12 +2,14 @@ from django.core.management.base import BaseCommand
 from polls.views import *
 from polls.models import *
 from time import time
+from datetime import datetime
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         start = time()
         api_key = os.environ.get('API_KEY')
         api = omdb.OMDBClient(apikey=api_key)
-        today_date = datetime.datetime.strptime(datetime.datetime.today().strftime('%Y-%m-%d'), "%Y-%m-%d").date()
+        today_date = datetime.strptime(datetime.today().strftime('%Y-%m-%d'), "%Y-%m-%d").date()
         series_pks = []
         series_set = Series.objects.filter(Verified=True)
 

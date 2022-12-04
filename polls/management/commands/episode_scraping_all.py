059922e06ef2925f25091bda826e58448d931ae2
Ahain5221 <dawkol1@st.amu.edu.pl>
2022-12-04 20:27:50 +0100

    epiose_scraping
    .

---
+++ polls/management/commands/episode_scraping_all.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from polls.views import *
+from polls.models import *
+from time import time
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        start = time()
+        api_key = os.environ.get('API_KEY')
+        api = omdb.OMDBClient(apikey=api_key)
+        today_date = datetime.datetime.strptime(datetime.datetime.today().strftime('%Y-%m-%d'), "%Y-%m-%d").date()
+        series_pks = []
+        series_set = Series.objects.filter(Verified=True)
+
+        for series in series_set.all():
+            if series.episodes_update_date:
+                days_difference = (today_date - series.episodes_update_date).days
+                if days_difference >= 7:
+                    series_pks.append(series.pk)
+            else:
+                series_pks.append(series.pk)
+        for series_pk in series_pks:
+            print(series_pk)
+            episodes_scraping(series_pk, api, start)
+            print(time() - start)
+            if time() - start > 25:
+                print('timeout')
+                exit()
+        exit()
